56494ca8947909f21ef2051b02891306169909a0
kst788 <karmasonam069@gmail.com>
2025-06-08 15:18:27 +0600

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,20 +5,6 @@ const db = require('../config/db');
 const sendEmail = require('../utils/sendEmail');
 const { getBaseUrl } = require('../utils/urlHelper');
 
-// Helper function to get base URL
-const getBaseUrl = (req) => {
-  // If BASE_URL is set in environment variables, use it
-  if (process.env.BASE_URL) {
-    return process.env.BASE_URL.replace(/\/$/, '');
-  }
-  // For production, force HTTPS
-  if (process.env.NODE_ENV === 'production') {
-    return `https://${req.get('host')}`;
-  }
-  // For development
-  return `${req.protocol}://${req.get('host')}`;
-};
-
 // GET: Signup Page
 exports.getSignup = (req, res) => {
   res.render('signup');
